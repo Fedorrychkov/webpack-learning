99142763034a2cf3f043b6947c6e4fdcf7d943ee
Fedor Rychkov <fedorrychkov@yandex.ru>
2023-12-10 10:56:40 +0300

    feat(#2): add webpack config for second lesson

--- 2/webpack.config.js
+++ 2/webpack.config.js
@@ -3,6 +3,17 @@ const path = require('node:path');
 module.exports = {
 	devtool: false,
 	mode: 'development',
-
 	// ...ваш конфиг
+
+	entry: {
+		app: { import: './src/app.js', dependOn: 'vendor' },
+		home: { import: './src/pages/home.js', dependOn: 'vendor' },
+		login: { import: './src/pages/login.js' },
+		vendor: ['react', 'react-dom'],
+	},
+	output: {
+		clean: true,
+		path: path.resolve('./build'),
+    filename: '[name].[contenthash].js',
+	}
 }
