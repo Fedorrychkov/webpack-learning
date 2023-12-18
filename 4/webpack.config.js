2ca38ab386f2c8b6ca3a9f9d2a7ba23c935b5132
Fedor Rychkov <fedorrychkov@yandex.ru>
2023-12-18 15:36:45 +0300

    feat(#4): add homework for webapck bundle

--- 4/webpack.config.js
+++ 4/webpack.config.js
@@ -4,6 +4,27 @@ module.exports = {
 	// не изменять!
 	devtool: false,
 	mode: 'development',
+	entry: {
+		app: { import: './src/app.js' },
+	},
+	output: {
+		clean: true,
+		path: path.resolve('./bundle'),
+    filename: 'awesome-bundle.js',
+	},
+	resolve: {
+		alias: {
+			Utils: path.resolve(__dirname, 'src/utilities/'),
+			Helpers: path.resolve(__dirname, 'src/helpers/'),
+			Components: path.resolve(__dirname, 'src/components/'),
+		},
+		fallback: {
+			coolurl: path.resolve(__dirname, 'src/utilities/url'),
+			'_/render': path.resolve(__dirname, 'src/helpers/render'),
+			'_/sum': path.resolve(__dirname, 'src/utilities/sum'),
+		},
+		extensions: ['.js', '.cooljson'],
+	},
 	module: {
 		rules: [
 			{
