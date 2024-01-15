bd8e14e8cb09de892f8b2213150c1410b7c3f501
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-01-15 22:22:07 +0300

    feat(#9): add server.js settings to wds

--- 9/server.js
+++ 9/server.js
@@ -1,5 +1,55 @@
 const express = require("express");
+const webpack = require("webpack");
+const middleware = require("webpack-dev-middleware");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
 const app = express();
+
+const compiler = webpack({
+  // webpack options
+	mode: 'development',
+  devtool: 'source-map',
+	entry: {
+		app: './src/app.js',
+		home: './src/pages/home.js',
+		login: './src/pages/login.js',
+	},
+
+	resolve: {
+		alias: {
+			components: path.resolve(__dirname, './src/components/'),
+			assets: path.resolve(__dirname, './src/assets/'),
+		},
+		extensions: ['.js', '.png'],
+	},
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'base.html',
+    }),
+  ],
+	module: {
+		rules: [
+      {
+        test: /\.(png)$/i,
+        type: 'asset',
+        generator: {
+					filename: '[name][ext]',
+        }
+      },
+    ]
+  },
+});
+
+app.use(
+  middleware(compiler, {
+    serverSideRender: false,
+    writeToDisk: false,
+    headers: [ {key: "X-custom-header", value: "foo"} ],
+  }),
+);
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Server listening at ${port}`));
+ 
